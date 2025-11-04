4af4cba5ee47a103e410e7fe280e8a41ad537a3b
zaka7024 <zaka7024@gmail.com>
2025-11-04 13:15:15 +0300

    Update default language

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,7 @@ import {defineRouting} from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'ar'],
  
-  defaultLocale: 'en',
+  defaultLocale: 'ar',
   localeCookie: {
     name: 'USER_LOCALE',
     maxAge: 60 * 60 * 24 * 365
